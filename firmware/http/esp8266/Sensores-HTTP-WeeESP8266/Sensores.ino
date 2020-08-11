d9198a00ae5d4c5c2966f4aa7eaac91cc47d57bd
Cadf <31243375+Cadf@users.noreply.github.com>
2020-08-11 15:08:27 -0300

    Create Sensores.ino

---
+++ firmware/http/esp8266/Sensores-HTTP-WeeESP8266/Sensores.ino
@@ -0,0 +1,71 @@
+// ***************************************************************************************************
+// *  Chaves de configuração dos sensores utilizados                                                 *
+// *                                                                                                 *
+// *  Desenvolvido por David Souza - SmartMosaic - smartmosaic.com.br                                *
+// *  Versão 1.0 - Junho/2020                                                                        *
+// *                                                                                                 *
+// ***************************************************************************************************
+
+// ***************************************************************************************************
+// *  Sensores que estão sendo utilizados                                                            *
+// ***************************************************************************************************
+
+#define USE_LM35      ON       // Ativa o uso do sensor LM35
+#define USE_DHT11     OFF       // Ativa o uso do sensor DHT11
+#define USE_DHT22     ON       // Ativa o uso do sensor DHT12
+
+// ***************************************************************************************************
+// *  Bibliotecas                                                                                    *
+// ***************************************************************************************************
+
+#if (USE_DHT11 == ON || USE_DHT22 == ON)
+  #include <SimpleDHT.h>
+#endif
+
+// ***************************************************************************************************
+// *  Definição da pinos utilizados nos sensores                                                     *
+// ***************************************************************************************************
+
+#if (USE_LM35 == ON)
+  const int PIN_LM35 = A2;    //PINO ANALÓGICO UTILIZADO PELO SENSOR LM35
+#endif
+
+#if (USE_DHT11 == ON || USE_DHT22 == ON)
+  #define PIN_DHT     6         // Pino de comunicação com o DHT
+#endif
+#if (USE_DHT11 == ON)
+  SimpleDHT11 dht11(PIN_DHT); 
+#elif (USE_DHT22 == ON)
+  SimpleDHT22 dht22(PIN_DHT); 
+#endif
+
+// ***************************************************************************************************
+// *  Função de leitura de sensores                                                                  *
+// ***************************************************************************************************
+void read_sensors(void)
+{
+    // Definição da Umidade
+    #if (HUMI == RAND)
+      humidity = random(VAL_MIN, VAL_MAX) * VAL_FAC;
+    #elif (HUMI == DHT11 && USE_DHT11 == ON)
+      dht11.read(&temperature, &humidity, NULL);
+    #elif (HUMI == DHT22 && USE_DHT22 == ON)
+      dht22.read2(&temperature, &humidity, NULL);
+    #else
+      humidity = 0;
+    #endif
+
+    // Definição da Temperatura
+    #if (TEMP == RAND)
+      temperature = random(VAL_MIN, VAL_MAX) * VAL_FAC;
+    #elif (TEMP == DHT11 && USE_DHT11 == ON)
+      dht11.read(&temperature, &humidity, NULL);
+    #elif (TEMP == DHT22 && USE_DHT22 == ON)
+      dht22.read2(&temperature, &humidity, NULL);
+    #elif (TEMP == LM35 && USE_LM35 == ON)
+      temperature = (float(analogRead(PIN_LM35)) * 5 / (1023)) / 0.01;
+    #else
+      temperature = 0;
+    #endif
+ 
+}
